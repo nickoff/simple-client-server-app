ca8b1e2d59976972c795b946735201915c8ea159
nickoff <nick.olehnovich@gmail.com>
2025-09-25 23:51:46 +0300

    feat: add react hook form controller

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -8,7 +8,7 @@ export const HomePage = () => {
     <main className="flex flex-col items-center gap-5">
       <h1>👋 Welcome!</h1>
       <p className="text-2xl max-w-96 text-center">We're so glad you're here. This space is designed for connection, support, and inspiration. Feel free to reach out — we're always ready to listen and help.</p>
-      <Button onClick={() => navigate('/message')}>Next</Button>
+      <Button size="large" type="primary" onClick={() => navigate('/message')}>Next</Button>
     </main>
   )
 }
--- frontend/src/components/SendMessagePage.tsx
+++ frontend/src/components/SendMessagePage.tsx
@@ -1,15 +1,21 @@
 import { Button, Input } from "antd"
 import TextArea from "antd/es/input/TextArea"
+import { Controller, useForm } from "react-hook-form"
 
 export const SendMessagePage = () => {
+  const {control, handleSubmit, reset} = useForm({mode: "onChange"})
+  const onSubmit = (data) => {
+    console.log(data)
+    reset()
+  }
 
   return (
     <main className="flex flex-col items-center gap-5">
       <h2 className="text-4xl">Send us your message</h2>
-      <form className="flex flex-col w-full items-center gap-3">
-        <Input name="name" size="large" type="text" placeholder="Enter your name"/>
-        <Input name="phone" size="large" type="text" placeholder="Enter your phone number"/>
-        <TextArea name="message" size="large" autoSize={{ minRows: 2, maxRows: 6 }} placeholder="Enter your message"/>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-full items-center gap-3">
+        <Controller control={control} name="name" defaultValue="" render={({field}) => <Input {...field} size="large" type="text" placeholder="Enter your name"/>} />
+        <Controller control={control} name="phone" defaultValue="" render={({field}) => <Input {...field} size="large" type="text" placeholder="Enter your phone number" />} />
+        <Controller control={control} name="message" defaultValue="" render={({field}) => <TextArea {...field} size="large" autoSize={{ minRows: 2, maxRows: 6 }} placeholder="Enter your message"/>} />
         <Button htmlType="submit" type="primary" size="large" variant="filled">Send message</Button>
       </form>
     </main>
